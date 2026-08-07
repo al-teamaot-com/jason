4b62f84a556c403b6887c84dcdd9edccd283392d
al-teamaot-com <al@teamaot.com>
2026-08-07 15:35:49 -0400

    Export CAP-004 Autotask signal producer

--- implementation/cap-004/src/jason_cap_004/__init__.py
+++ implementation/cap-004/src/jason_cap_004/__init__.py
@@ -1,8 +1,10 @@
+from .autotask_signals import AutotaskOperationalSignalProducer
 from .models import AttentionItem, OperationalBriefing, OperationalSignal
 from .service import OperationalBriefingService
 
 __all__ = [
     "AttentionItem",
+    "AutotaskOperationalSignalProducer",
     "OperationalBriefing",
     "OperationalBriefingService",
     "OperationalSignal",
