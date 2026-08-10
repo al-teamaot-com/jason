fffcf19395362cbfc9405343ece735e3e7c90de7
al-teamaot-com <al@teamaot.com>
2026-08-10 15:43:41 -0400

    Expose CAP-004 email send interfaces

---
+++ implementation/cap-004/src/jason_cap_004/__init__.py
@@ -0,0 +1,35 @@
+from .ses import AwsSesConfig, AwsSesTransport, SesTransportError
+from .service import (
+    CAPABILITY_NAME,
+    SES_PROVIDER_ID,
+    SES_SECRET_NAME,
+    EmailCapabilityError,
+    EmailMessage,
+    EmailProviderMismatchError,
+    EmailSecretError,
+    EmailSenderDeniedError,
+    EmailSendPolicy,
+    EmailValidationError,
+    GovernedEmailSendInvoker,
+    ProviderSendResult,
+    SecretLease,
+)
+
+__all__ = [
+    "AwsSesConfig",
+    "AwsSesTransport",
+    "CAPABILITY_NAME",
+    "EmailCapabilityError",
+    "EmailMessage",
+    "EmailProviderMismatchError",
+    "EmailSecretError",
+    "EmailSenderDeniedError",
+    "EmailSendPolicy",
+    "EmailValidationError",
+    "GovernedEmailSendInvoker",
+    "ProviderSendResult",
+    "SES_PROVIDER_ID",
+    "SES_SECRET_NAME",
+    "SecretLease",
+    "SesTransportError",
+]
