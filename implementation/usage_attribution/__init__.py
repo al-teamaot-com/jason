07d44ae5449fb07607024040a6ac40732a6193c7
al-teamaot-com <al@teamaot.com>
2026-09-09 11:36:17 -0400

    Export usage attribution contracts

---
+++ implementation/usage_attribution/__init__.py
@@ -0,0 +1,17 @@
+"""Jason usage attribution primitives."""
+
+from .contracts import (
+    ActorType,
+    AttributionContext,
+    BillingClass,
+    TelemetryQuality,
+    UsageAttributionEntry,
+)
+
+__all__ = [
+    "ActorType",
+    "AttributionContext",
+    "BillingClass",
+    "TelemetryQuality",
+    "UsageAttributionEntry",
+]
