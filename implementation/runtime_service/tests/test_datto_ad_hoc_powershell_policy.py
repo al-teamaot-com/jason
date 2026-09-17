f7c41baa230117f1442f13d78a5aaaad5900f75a
al-teamaot-com <al@teamaot.com>
2026-09-17 11:05:44 -0400

    Test dynamic passive and active Datto diagnostics

--- implementation/runtime_service/tests/test_datto_ad_hoc_powershell_policy.py
+++ implementation/runtime_service/tests/test_datto_ad_hoc_powershell_policy.py
@@ -8,8 +8,16 @@ from connectors.datto_rmm.component_execution import (
     StaticComponentAllowlist,
 )
 from jason_runtime.datto_component_scope import (
+    DATTO_APPROVAL_MODE_PER_RUN,
+    DATTO_APPROVAL_MODE_STANDING_SAFE,
+    DATTO_DIAGNOSTIC_CLASS_ACTIVE_PROBE,
+    DATTO_DIAGNOSTIC_CLASS_APPROVAL_REQUIRED,
+    DATTO_DIAGNOSTIC_CLASS_PASSIVE_READ,
     DATTO_EXECUTION_COMPONENTS_JSON_ENV,
+    DattoApprovedComponent,
+    classify_datto_powershell_diagnostic_command,
     configured_datto_components,
+    effective_datto_component_approval_mode,
 )
 
 
@@ -42,6 +50,21 @@ def policy_for(
             entries=(entry,)
         )
     )
+
+
+def component():
+    return DattoApprovedComponent(
+        uid=UID,
+        name=NAME,
+        approval_mode=DATTO_APPROVAL_MODE_PER_RUN,
+    )
+
+
+def effective_mode(command):
+    return effective_datto_component_approval_mode(
+        component(),
+        {"usrInput": command},
+    )[0]
 
 
 def test_exact_powershell_component_accepts_usrinput():
@@ -112,7 +135,7 @@ def test_same_name_with_unreviewed_uid_cannot_use_usrinput():
         )
 
 
-def test_powershell_component_cannot_be_standing_safe(
+def test_powershell_component_cannot_be_configured_standing_safe(
     monkeypatch,
 ):
     monkeypatch.setenv(
@@ -138,3 +161,96 @@ def test_powershell_component_cannot_be_standing_safe(
         components[0].requires_explicit_approval
         is True
     )
+
+
+@pytest.mark.parametrize(
+    "command",
+    [
+        "Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object Caption,Version,LastBootUpTime",
+        "Get-WinEvent -LogName System -MaxEvents 20 | Select-Object Id,TimeCreated,ProviderName",
+        r"reg.exe query 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion' /v ProductName",
+        "netsh wlan show interfaces",
+        "ipconfig /all",
+        "sc.exe query HUNTAgent",
+        "pnputil.exe /enum-drivers",
+    ],
+)
+def test_previously_unseen_passive_diagnostics_can_run_standing_safe(command):
+    assert (
+        classify_datto_powershell_diagnostic_command(command)
+        == DATTO_DIAGNOSTIC_CLASS_PASSIVE_READ
+    )
+    assert effective_mode(command) == DATTO_APPROVAL_MODE_STANDING_SAFE
+
+
+@pytest.mark.parametrize(
+    "command",
+    [
+        "Test-NetConnection 1.1.1.1 -Port 443",
+        "Resolve-DnsName example.com",
+        "ping.exe 1.1.1.1 -n 2 -w 1000",
+        "tracert.exe example.com -h 10 -w 1000",
+        "nslookup.exe example.com",
+    ],
+)
+def test_bounded_active_diagnostic_probes_can_run_standing_safe(command):
+    assert (
+        classify_datto_powershell_diagnostic_command(command)
+        == DATTO_DIAGNOSTIC_CLASS_ACTIVE_PROBE
+    )
+    assert effective_mode(command) == DATTO_APPROVAL_MODE_STANDING_SAFE
+
+
+@pytest.mark.parametrize(
+    "command",
+    [
+        "Restart-Service HUNTAgent",
+        "Set-ItemProperty HKLM:\\Software\\AOT -Name Test -Value 1",
+        "netsh wlan delete profile name=AlphaGuest",
+        r"reg.exe add HKLM\Software\AOT /v Test /d 1",
+        "ipconfig /flushdns",
+        "sc.exe stop HUNTAgent",
+        "shutdown.exe /r /t 0",
+        "Get-SomethingNew -Name Example",
+        "ping.exe 1.1.1.1 -t",
+    ],
+)
+def test_mutating_unknown_or_unbounded_commands_still_require_approval(command):
+    assert (
+        classify_datto_powershell_diagnostic_command(command)
+        == DATTO_DIAGNOSTIC_CLASS_APPROVAL_REQUIRED
+    )
+    assert effective_mode(command) == DATTO_APPROVAL_MODE_PER_RUN
+
+
+@pytest.mark.parametrize(
+    "command",
+    [
+        r"Get-ItemProperty 'HKLM:\SAM\Domains\Account'",
+        r"reg.exe query HKLM\SECURITY\Policy\Secrets",
+        "netsh wlan show profile name=AlphaPsych key=clear",
+        "Get-CimInstance Win32_Process | Select-Object Name,CommandLine",
+    ],
+)
+def test_secret_or_credential_oriented_reads_do_not_auto_pass(command):
+    assert (
+        classify_datto_powershell_diagnostic_command(command)
+        == DATTO_DIAGNOSTIC_CLASS_APPROVAL_REQUIRED
+    )
+    assert effective_mode(command) == DATTO_APPROVAL_MODE_PER_RUN
+
+
+def test_command_chaining_and_remote_management_scope_fail_closed():
+    commands = (
+        "Get-Service; Restart-Service HUNTAgent",
+        "Get-Service -ComputerName server01",
+        "Get-CimInstance Win32_OperatingSystem -ComputerName server01",
+        "Get-Service | ForEach-Object Stop-Service",
+    )
+
+    for command in commands:
+        assert (
+            classify_datto_powershell_diagnostic_command(command)
+            == DATTO_DIAGNOSTIC_CLASS_APPROVAL_REQUIRED
+        )
+        assert effective_mode(command) == DATTO_APPROVAL_MODE_PER_RUN
