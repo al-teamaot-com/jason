f2c0f22c72990e2c0d9c6e4405bd01d9884877ff
al-teamaot-com <al@teamaot.com>
2026-09-21 14:01:23 -0400

    Add KFS connector package

---
+++ implementation/connectors/kfs/__init__.py
@@ -0,0 +1,4 @@
+from connectors.kfs.connector import KfsConnector
+from connectors.kfs.mutations import KfsMutationConnector, KfsMutationExecutor
+
+__all__ = ["KfsConnector", "KfsMutationConnector", "KfsMutationExecutor"]
